547cc0030f7ce682375a17acaea6353ff0cbe233
thaibinh <thienthan1455@gmail.com>
2019-10-15 17:49:57 +0700

    update

---
+++ ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Account
+    {
+        private int i;
+        public Account(int money)
+        {
+            i = money;
+        }
+        public void Deposit(int money)
+        {
+            i += money;
+        }
+        public void Withdraw(int money)
+        {
+            i -= money;
+        }
+        public int Balance()
+        {
+            return i;
+        }
+
+    }
+}
---
+++ ConsoleApplication1/UnitTestAccount/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestAccount
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Account money = new Account(200000);
+
+            Assert.IsNotNull(money);
+            Assert.IsInstanceOfType(money, typeof(Account));
+            Assert.AreEqual(200000, money.Balance());
+        }
+        
+        
+    }
+}
